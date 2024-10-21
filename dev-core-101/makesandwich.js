e01f1b386b78b2711fb9178707c3bc34443f3917
aidasuen <aidas2211@gmail.com>
2024-10-21 20:35:43 +0500

    Add files via upload

---
+++ dev-core-101/makesandwich.js
@@ -0,0 +1,50 @@
+function makesandwich() { 
+let bread = "хлеб";
+let filling = "начинка"; //"сливочное масло", "сыр", "колбаса"
+let equipment = "оборудование"; //"холодильник","хлебница"
+// 1. Открываем холодильник
+ console.log("Открываем" + equipment);
+ // 2. Находим сливочное масло
+console.log("Находим" + filling);
+// 3. Находим колбасу
+console.log("Находим" + filling);
+// 4. Находим сыр
+console.log("Находим" + filling);
+// 5. Достаем колбасу
+console.log("Достаем" + filling);
+// 6. Достаем сыр
+console.log("Достаем" + filling);
+// 7. Достаем сливочное масло
+console.log("Достаем" + filling);
+// 8. Берем колбасу сыр и сливочное масло
+console.log("Берем" + filling + filling + filling);
+// 9. Ложим на столешницу колбасу сыр и сливочное масло
+console.log(" Кладем " + equipment + filling + filling + filling);
+// 10.Закрываем холодильник
+console.log("Закрываем" + equipment);
+// 11. Ищем хлебницу
+console.log("Ищем" + equipment);
+// 12. Открываем хлебницу
+console.log("Открываем" + equipment);
+// 13. Достаем хлеб
+console.log("Достаем" + bread);
+// 14. Берем со столешницы сливочное масло
+console.log("Берем со" + equipment + filling); 
+// 15. Намазываем сливочное масло на хлеб
+console.log("Намазываем" + filling +bread); 
+// 16. Берем колбасу со столешницы
+console.log("Берем" + filling + equipment);
+// 17. На доске разрезаем колбасу на слайсы
+console.log(" разрезаем " + equipment + filling);
+// 18. Ложим на хлеб поверх сливочного масла
+console.log("Кладем" + bread + filling);
+// 19. Берем сыр со столешницы
+console.log("Берем" + filling + equipment);
+// 20. На доске разрезаем сыр на слайсы
+console.log( "разрезаем "  + equipment + filling);
+// 21. Кладем поверх сливочного масла и колбасы
+console.log("Кладем " + filling + filling);
+// 22. Кушаем
+console.log("Кушаем");  
+}
+makesandwich()
