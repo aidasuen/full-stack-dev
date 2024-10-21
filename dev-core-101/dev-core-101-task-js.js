08e1fe734caa626ed2bcf79ea18cc354d097c04b
aidasuen <aidas2211@gmail.com>
2024-10-22 00:54:43 +0500

    Update dev-core-101-task-js.js

--- dev-core-101/dev-core-101-task-js.js
+++ dev-core-101/dev-core-101-task-js.js
@@ -1,50 +1,49 @@
 function makesandwich() { 
 let bread = "хлеб";
-let filling = "начинка"; //"сливочное масло", "сыр", "колбаса"
-let equipment = "оборудование"; //"холодильник","хлебница"
+let filling = "сливочное масло"; 
+let equipment = "холодильник"; 
+let filling1 = "колбасу";
+let filling2 = "сыр";
+let equipment1 ="хлебницу";
+let equipment2 ="столещницу"
+let equipment3 = "доске"
 // 1. Открываем холодильник
- console.log("Открываем" + equipment);
+ console.log(`Открываем  ${equipment}`);
  // 2. Находим сливочное масло
-console.log("Находим" + filling);
+console.log(`Находим  ${filling}`);
 // 3. Находим колбасу
-console.log("Находим" + filling);
+console.log(`Находим  ${filling1}`);
 // 4. Находим сыр
-console.log("Находим" + filling);
+console.log(`Находим  ${filling2}` );
 // 5. Достаем колбасу
-console.log("Достаем" + filling);
+console.log(`Достаем ${filling1}`);
 // 6. Достаем сыр
-console.log("Достаем" + filling);
+console.log(`Достаем  ${filling2}`);
 // 7. Достаем сливочное масло
-console.log("Достаем" + filling);
-// 8. Берем колбасу сыр и сливочное масло
-console.log("Берем" + filling + filling + filling);
-// 9. Ложим на столешницу колбасу сыр и сливочное масло
-console.log(" Кладем " + equipment + filling + filling + filling);
-// 10.Закрываем холодильник
-console.log("Закрываем" + equipment);
-// 11. Ищем хлебницу
-console.log("Ищем" + equipment);
-// 12. Открываем хлебницу
-console.log("Открываем" + equipment);
-// 13. Достаем хлеб
-console.log("Достаем" + bread);
-// 14. Берем со столешницы сливочное масло
-console.log("Берем со" + equipment + filling); 
-// 15. Намазываем сливочное масло на хлеб
-console.log("Намазываем" + filling +bread); 
-// 16. Берем колбасу со столешницы
-console.log("Берем" + filling + equipment);
-// 17. На доске разрезаем колбасу на слайсы
-console.log(" разрезаем " + equipment + filling);
-// 18. Ложим на хлеб поверх сливочного масла
-console.log("Кладем" + bread + filling);
-// 19. Берем сыр со столешницы
-console.log("Берем" + filling + equipment);
-// 20. На доске разрезаем сыр на слайсы
-console.log( "разрезаем "  + equipment + filling);
-// 21. Кладем поверх сливочного масла и колбасы
-console.log("Кладем " + filling + filling);
-// 22. Кушаем
+console.log(`Достаем  ${filling}`);
+// 8. Кладем на столешницу колбасу, сыр и сливочное масло
+console.log(`Кладем на  ${equipment2} ${filling1}, ${filling2} и ${filling}`);
+// 9.Закрываем холодильник
+console.log(`Закрываем ${equipment}`);
+// 10. Открываем хлебницу
+console.log(`Открываем ${equipment1}`);
+// 11. Достаем хлеб
+console.log(`Достаем ${bread}`);
+// 12. Берем сливочное масло
+console.log(`Берем ${filling}`); 
+// 13. Намазываем сливочное масло на хлеб
+console.log(`Намазываем ${filling} на ${bread}`); 
+// 14. Берем колбасу 
+console.log(`Берем ${filling1}`);
+// 15. На доске разрезаем колбасу на слайсы
+console.log(`Разрезаем на ${equipment3} ${filling1} на слайсы`);
+// 16. Берем сыр
+console.log(`Берем ${filling2}`);
+// 17. Разрезаем сыр на слайсы
+console.log(`Разрезаем ${filling2} на слайсы`);
+// 18. Кладем сыр и колбасу на хлеб
+console.log(`Кладем ${filling2} и ${filling1} на ${bread}`);
+// 19. Кушаем
 console.log("Кушаем");  
 }
 makesandwich()
