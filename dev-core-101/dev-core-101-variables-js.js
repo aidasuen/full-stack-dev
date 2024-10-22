c275222abef8fd33c2068d6258d1f779a688a75a
Aida <aidas2211@gmail.com>
2024-10-23 01:03:43 +0500

    add

---
+++ dev-core-101/dev-core-101-variables-js.js
@@ -0,0 +1,7 @@
+let a=20
+let b=10
+console.log(a + b);  // 30
+console.log(a - b);  // 10
+let name = "Всем";
+let greeting = "Привет, " + name;
+console.log(greeting);  // Привет, Всем
---
+++ dev-core-101/dev-core-101-variables-python.py
@@ -0,0 +1,7 @@
+a=6
+b=5
+print(a + b) # 11
+print(a - b) # 1
+name = "всем"
+greeting = "привет," + name
+print (greeting) #привет всем
