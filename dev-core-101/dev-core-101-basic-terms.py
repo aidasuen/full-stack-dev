febbd61ee149d7ef9d778b6215d7e2151833c7e3
Aida <aidas2211@gmail.com>
2024-10-26 16:29:16 +0500

    added

---
+++ dev-core-101/dev-core-101-basic-terms.py
@@ -0,0 +1,34 @@
+#variable 
+height = 154
+
+#function
+def add_numbers(num1, num2):
+    return num1 + num2  
+
+#if
+if a > b:
+    print(f"{a} больше, чем {b}.")
+else:
+    print(f"{a} не больше, чем {b}.")
+    
+#loop
+for i in range(1, 6):  
+    square = i ** 2
+    print(f"Квадрат числа {i} равен {square}.")
+
+#оператор присваивания
+x=3
+
+#выражение
+  return num ** 2
+
+#целые числа
+ number=7
+
+#comments
+
+#data types
+isactive = False
+
+#strings
+fruits = ["яблоко", "банан", "апельсин"]
